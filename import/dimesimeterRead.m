fbc8477bab507c15c60cd5b7dbca2ad7816ed393
Bart te Lindert <btlindert@users.noreply.github.com>
2015-04-30 13:03:28 +0200

    Added additional output variables

--- import/dimesimeterRead.m
+++ import/dimesimeterRead.m
@@ -1,4 +1,4 @@
-function [lux, act] = dimesimeterRead(filename)
+function [lux, cla, cs, act, x, y] = dimesimeterRead(filename)
 % DIMESIMETERREAD loads data from a Dimesimeter txt file 
 % filename is a  _dimeprocessed.txt file with 7 columns:
 % [Time	Lux	CLA	CS	Activity	x	y]
@@ -7,8 +7,14 @@ function [lux, act] = dimesimeterRead(filename)
 %   filename - path to dimesimeter file 
 %
 % Results:
-%   lux - timesseries of lux 
+%   lux - timeseries of lux 
+%   cla - timeseries of circadian lux
+%   cs  - timeseries of circadian stimulus
 %   act - timeseries of activity counts
+%   x   - timeseries of x activity
+%   y   - timeseries of y activity
+%
+% For more details on CS see: Rea et al. 2005 213.
 %
 % Copyright (c) 2014 Bart te Lindert
 
@@ -17,20 +23,42 @@ fid    = fopen(filename);
 C      = textscan(fid, format, 'delimiter', '\t', 'headerlines', 1); 
 fclose(fid);
 
-time     = C{1,1};
-lux      = C{1,2};
-activity = C{1,5};
-
-% Convert strings to nums/doubles.
-time = datenum(time, 'HH:MM:SS mm/dd/yy');
-lux  = str2double(lux);
-act  = str2double(activity);
+time = datenum(C{1,1}, 'HH:MM:SS mm/dd/yy'); 
+lux  = str2double(C{1,2});
+cla  = str2double(C{1,3});
+cs   = str2double(C{1,4});
+act  = str2double(C{1,5});
+x    = str2double(C{1,6});
+y    = str2double(C{1,7});
 
 % Create timeseries.
+% Lux.
 lux = timeseries(lux, time, 'Name', 'LUX');
 lux.DataInfo.Unit  = 'lux';
 lux.TimeInfo.Units = 'minutes';
 
+% Circadian lux.
+cla = timeseries(cla, time, 'Name', 'CLA');
+cla.DataInfo.Unit  = 'Weighted lux';
+cla.TimeInfo.Units = 'minutes';
+
+% Circadian stimulus.
+cs = timeseries(cs, time, 'Name', 'CS');
+cs.DataInfo.Unit  = 'Weighted W/m^2';
+cs.TimeInfo.Units = 'minutes';
+
+% Activity.
 act = timeseries(act, time, 'Name', 'ACT');
 act.DataInfo.Unit  = 'counts';
 act.TimeInfo.Units = 'minutes';
+
+% X.
+x = timeseries(x, time, 'Name', 'X');
+x.DataInfo.Unit  = 'lux';
+x.TimeInfo.Units = 'minutes';
+
+% Y.
+y = timeseries(y, time, 'Name', 'Y');
+y.DataInfo.Unit  = 'lux';
+y.TimeInfo.Units = 'minutes';
+
