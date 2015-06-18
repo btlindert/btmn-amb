81ab3200baac7de88853955cdd8c12fa08b4a750
Bart te Lindert <btlindert@users.noreply.github.com>
2015-06-18 10:03:58 +0200

    Modified script to allow batch processing using OGE

--- humidity/analyzeAmbientHumidity.m
+++ humidity/analyzeAmbientHumidity.m
@@ -1,3 +1,5 @@
+function analyzeAmbientHumidity(SUBJECT)
+
 % analyzeAmbientHumidity analyzes the humidity data from the iButton hydrochron
 % sensors at the coat and sweater.
 
@@ -9,101 +11,140 @@
 
 PATH            = '/data1/recordings/btmn/subjects/';
 SUB_PATH        = '/humidity/raw/';
-PATH_TIMESTAMPS = '/data1/recordings/btmn/import/150430_behavior_blindert/';
+PATH_TIMESTAMPS = '/data1/recordings/btmn/import/';%150430_behavior_blindert/';
 OUTPUT_FOLDER   = '/data2/projects/btmn/analysis/amb/ambient-humidity/';
-MISSING         = [5, 7, 10, 17, 18, 21, 29, 39];
-ALL             = 1:44;
-SUBJECTS        = setdiff(ALL, MISSING);
 
-for iSubject = SUBJECTS
-    
-    % Subject string.
-    SUBJECT = sprintf('%04.0f', iSubject);
 
-    % Path to timestamps.
-    TIMESTAMPS = [PATH_TIMESTAMPS 'btmn_' SUBJECT '_behavior_mobile_timestamps.csv'];
+% Force input to be string
+SUBJECT = char(SUBJECT);
 
-    % Load all the timestamps for this subject.
-    [id, subjectId, alarmLabels, alarmCounter, formLabels, alarmTimestamps] ...
-        = timestampRead(TIMESTAMPS);
-    
-    % Set vars to empty or remove.
-    OUTER = '';
-    INNER = '';        
-    clear('humInner');
-    clear('humOuter');
-    
-    % Find the files (either .csv or .txt files).
-    fp = dir([PATH SUBJECT SUB_PATH]);
-    
-    % INNER.
-    f = regexpi({fp.name}, ['.*' SUBJECT '.*sweater.*'], 'match');
-    f = [f{:}]; 
-    if ~isempty(f)
-        INNER = [PATH SUBJECT SUB_PATH f{1}];
+% Recursively find path to timestamps file.
+files = subdir([PATH_TIMESTAMPS, 'btmn_' SUBJECT '_behavior_mobile_timestamps.csv']);
+
+% Proceed if there is only 1 file.
+if size(files, 1) == 1
+
+    TIMESTAMPS = files(1).name;
+
+    % Only proceed if the timestamps file exists as a file.
+    if exist(TIMESTAMPS, 'file') ~= 2
+
+        return 
+
     end
-    
-    % OUTER.
-    f = regexpi({fp.name}, ['.*' SUBJECT '.*coat.*'], 'match');
-    f = [f{:}];
-    if ~isempty(f)
-        OUTER = [PATH SUBJECT SUB_PATH f{1}];
-    end
-    
-    % Load iButton data from coat and sweater.
-    if ~isempty(INNER)
-        humInner = ibuttonHumidityRead(INNER);
-    end
-    
-    if ~isempty(OUTER)
-        humOuter = ibuttonHumidityRead(OUTER);
-    end
-    
-     % Open file and write headers.
+
+else
+
+    error('No or multiple timestamp files for subject %s', SUBJECT)
+
+end
+
+
+% Load all the timestamps for this subject.
+[~, ~, alarmLabels, alarmCounter, formLabels, alarmTimestamps] ...
+    = timestampRead(TIMESTAMPS);
+
+
+% Set vars to empty or remove.
+OUTER = '';
+INNER = '';        
+
+
+% INNER.
+files = subdir([PATH SUBJECT SUB_PATH '*sweater.*']);
+
+if size(files, 1) == 1
+
+    INNER    = files(1).name;
+    humInner = ibuttonHumidityRead(INNER);
+
+end    
+
+% OUTER.
+files = subdir([PATH SUBJECT SUB_PATH '*coat.*']); 
+
+if size(files, 1) == 1
+
+    OUTER    = files(1).name;
+    humOuter = ibuttonHumidityRead(OUTER);
+
+end
+
+% If either file exists, proceed.     
+if ~isempty(INNER) || ~isempty(OUTER)
+
+    % Open file and write headers.
     fid = fopen([OUTPUT_FOLDER 'btmn_' SUBJECT '_ambient-humidity_features.csv'], 'w');
     fprintf(fid, [repmat('%s, ', 1, 8), '%s\n'],...
         'subjectId', 'alarmCounter', 'alarmLabel', 'formLabel', ...
         'alarmTime', 'startTime', 'endTime', ...
-        'meanHumidityInner', 'meanHumidityOuter');              
+        'meanHumidityInner60', 'meanHumidityInner45', 'meanHumidityInner30', 'meanHumidityInner15', 'meanHumidityInner0', ...
+        'meanHumidityOuter60', 'meanHumidityOuter45', 'meanHumidityOuter30', 'meanHumidityOuter15', 'meanHumidityOuter0');              
     fclose(fid);
-    
+
+
     for iStamp = 1:numel(alarmTimestamps)
-    
+
         % Alarm timestamp.
         alarmTime = alarmTimestamps(iStamp);
+
+        % Declare vars.
+        meanHumidityInner = [];
+        meanHumidityOuter = [];  
         
-        % Get 20 minute period of data around the phone alarms;
-        % Add 5 min; subtract 15 min.
-        startTime = addtodate(alarmTime, -15, 'minute');
-        endTime   = addtodate(alarmTime, 5, 'minute');
+        % Onset and offset of analysis periods.
+        onset  = [-60, -45, -30, -15, 0];
+        offset = [-45, -30, -15, 0, 5];
         
-        if ~isempty(INNER)
-            humidityInnerData = getsampleusingtime(humInner, startTime, endTime);
-            meanHumidityInner = mean(humidityInnerData);
-        else
-            meanHumidityInner = [];
+        
+        for timeSlot = 1:5
+            
+            % Get 15 minute periods of data prior to the phone alarms
+            % plus 5 minutes during the task
+            startTime = addtodate(alarmTime, onset(timeSlot), 'minute');
+            endTime   = addtodate(alarmTime, offset(timeSlot), 'minute');
+
+            if ~isempty(INNER)
+
+                humidityInnerData = getsampleusingtime(humInner, startTime, endTime);
+                meanHumidityInner(timeSlot) = mean(humidityInnerData);
+
+            else
+
+                meanHumidityInner(timeSlot) = [];
+
+            end
+
+
+            if ~isempty(OUTER)
+
+                humidityOuterData = getsampleusingtime(humOuter, startTime, endTime);
+                meanHumidityOuter(timeSlot) = mean(humidityOuterData);  
+
+            else
+
+                meanHumidityOuter(timeSlot) = [];
+
+            end
+
         end
         
-        if ~isempty(OUTER)
-            humidityOuterData = getsampleusingtime(humOuter, startTime, endTime);
-            meanHumidityOuter = mean(humidityOuterData);        
-        else
-            meanHumidityOuter = [];
-        end
         
         % Write data to txt file.
         alarmLabel = alarmLabels{iStamp};
         formLabel = formLabels{iStamp};
-        
+
         fid = fopen([OUTPUT_FOLDER 'btmn_' SUBJECT '_ambient-humidity_features.csv'], 'a');
-        fprintf(fid, '%4.0f, %4.0f, %s, %s, %s, %s, %s, %4.2f, %4.2f\n', ...
-                 iSubject, alarmCounter(iStamp), alarmLabel, formLabel, ...
+        fprintf(fid, ['%s, %4.0f, %s, %s, %s, %s, %s,', repmat('%4.2f, ', 1, 9), '%4.2f\n'], ...
+                 SUBJECT, alarmCounter(iStamp), alarmLabel, formLabel, ...
                  datestr(alarmTime, 'dd-mm-yyyy HH:MM'), ...
                  datestr(startTime, 'dd-mm-yyyy HH:MM'), ...
                  datestr(endTime, 'dd-mm-yyyy HH:MM'), ...
                  meanHumidityInner, meanHumidityOuter);
         fclose(fid);
-             
+
     end
     
+end
+
 end
