40036d959d00a2332d4acb3af4a9c6374ba30cda
Bart te Lindert <btlindert@users.noreply.github.com>
2015-06-24 11:29:48 +0200

    Removed files

--- humidity/ambientHumidity.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#$ -N humidity
-#$ -S /bin/sh
-#$ -j y
-#$ -q veryshort.q
-#$ -o /data2/projects/btmn/analysis/amb/humidity.log
-#$ -u blindert
-matlab -nodesktop -nosplash -nodisplay -r "try analyzeAmbientHumidity('$1'); catch; end; quit"
