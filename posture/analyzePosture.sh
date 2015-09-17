e1cb3d0668411d16c530de7a2becb123eda9feac
Bart te Lindert <btlindert@users.noreply.github.com>
2015-09-17 16:57:58 +0200

    Move to long.q due to frequent crashes on verylong.q

--- posture/analyzePosture.sh
+++ posture/analyzePosture.sh
@@ -2,7 +2,7 @@
 #$ -N posture
 #$ -S /bin/sh
 #$ -j y
-#$ -q verylong.q
+#$ -q long.q
 #$ -o /data2/projects/btmn/analysis/amb/logs/posture.log
 #$ -u blindert
 matlab -nodesktop -nosplash -nodisplay -r "try analyzePosture('$1'); catch; end; quit"
