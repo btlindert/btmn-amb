d7b356f232b01012785ab9f3c438b337524489f8
btlindert <btlindert@users.noreply.github.com>
2017-03-08 16:10:12 +0100

    File/dir checking improved

--- main.sh
+++ main.sh
@@ -1,7 +1,9 @@
 #! /bin/sh
 
-# Folder specification.
-baseFolder="/data2/projects/btmn/analysis/amb"
+# Folder and date specification.
+baseDir="/someren/projects/btmn/analysis/amb"
+date="170308"
+
 modality=("ambient-humidity" 
 "ambient-light" 
 "ambient-temperature" 
@@ -11,7 +13,6 @@ modality=("ambient-humidity"
 "activity" 
 "posture" 
 "sleep")
-modality=("sleep")
 
 scriptName=("analyzeAmbientHumidity" 
 "analyzeAmbientLight" 
@@ -22,25 +23,45 @@ scriptName=("analyzeAmbientHumidity"
 "analyzeActivity" 
 "analyzePosture" 
 "analyzeSleep")
-scriptName=("analyzeSleep")
+
+# Add ecg...
+
+# Override with single modalities if required.
+#modality=("ambient-humidity")
+#scriptName=("analyzeAmbientHumidity")
 
 # Subtract 1 from number of elements in modality, because first index is 0.
 nModalities=$((${#modality[@]}-1))
 
-for subject in {0001..113}; 
+for i in $(seq 0 $nModalities);
 do
+    
+    # Check if the output directory exists, if not create.
+    if ! [ -d "${baseDir}/${modality[i]}/${date}" ]
+    then
+    
+        # Create directory
+        mkdir ${baseDir}/${modality[i]}/${date}
+            
+    fi
+    
+    # Check if the log directory exists, if not create.
+    if ! [ -d "${baseDir}/${modality[i]}/logs" ]
+    then
+    
+        # Create directory
+        mkdir ${baseDir}/${modality[i]}/logs
+        
+    fi
 
-	for i in $(seq 0 $nModalities);
-	do 
-		
-		# Check if output features file exists, if not run analysis. 
-		if ! [ -a "${baseFolder}/${modality[i]}/btmn_${subject}_${modality[i]}_features.csv" ]
+    for subject in {0001..1}; 
+    do
+
+        # Check if output features file exists, if not run analysis. 
+		if ! [ -a "${baseDir}/${modality[i]}/${date}/btmn_${subject}_${modality[i]}_features.csv" ]
 		then
-			# Submit job.
-			qsub ./${modality[i]}/${scriptName[i]}.sh $subject
-		fi	
-		echo 
-		
-	done
-	
+		    # Submit job.
+			qsub ./${modality[i]}/${scriptName[i]}.sh $subject $date
+		fi	 
+    done
 done
